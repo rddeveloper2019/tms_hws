736057a856869e09da5ab0e47d39b88505201289
Rid Van <fatzey2010@gmail.com>
2025-04-06 19:26:33 +0300

    sadd paginaition to get method

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 - добавить в cli команду edit (возможность редактирования записи)
-- ``` node app.js edit 5 'updated task' ```s
+- ``` node app.js edit 5 'updated task' ```
 
 
 - добавить в server в GET метод пагинацию
+- ``` http://localhost:3000/notes?limit=4&page=3  ```
+- ``` {
+    "page": 3,
+    "limit": 4,
+    "total": 30,
+    "totalPages": 8,
+    "data": [
+        {
+            "id": 9,
+            "title": "Посмотреть фильм"
+        },
+        {
+            "id": 10,
+            "title": "Сделать уборку"
+        },
+        {
+            "id": 11,
+            "title": "Изучить новый язык"
+        },
+        {
+            "id": 12,
+            "title": "Записаться на курс"
+        }
+      ]
+  }  
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const { addNote, deleteNode, updateNote, getNotes } = require('./noteManager');
+const { addNote, deleteNode, updateNote, getNotes } = require("./noteManager");
 
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 // GET
 // POST
 // DELETE
@@ -14,94 +14,111 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   const path = parsedUrl.pathname;
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if(method === 'GET' && path === '/notes'){
+  if (method === "GET" && path === "/notes") {
     const notes = getNotes();
-    res.writeHead(200, { 'content-type': 'application/json'});
-    res.end(JSON.stringify(notes));
-  }
+    res.writeHead(200, { "content-type": "application/json" });
 
-  else if(method === 'POST' && path === '/notes'){
-    let body = '';
+    let limit = parseInt(parsedUrl.query?.limit, 10);
+    let page = parseInt(parsedUrl.query?.page, 10);
 
-    req.on('data', chunk => {
+    if (isNaN(limit) || limit <= 0) {
+      limit = notes.length; // или можно установить дефолтное значение, например 10
+    }
+
+    if (isNaN(page) || page < 1) {
+      page = 1;
+    }
+
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    const data = notes.slice(start, end);
+
+    res.end(
+      JSON.stringify({
+        page: Number(page),
+        limit: Number(limit),
+        total: notes.length,
+        totalPages: Math.ceil(notes.length / limit),
+        data,
+      }),
+    );
+  } else if (method === "POST" && path === "/notes") {
+    let body = "";
+
+    req.on("data", (chunk) => {
       body += chunk;
     });
 
-    req.on('end', () => {
+    req.on("end", () => {
       try {
         const { title } = JSON.parse(body);
-        if(!title){
-          throw new Error('title is required');
+        if (!title) {
+          throw new Error("title is required");
         }
         const newNote = addNote(title);
-        res.writeHead(201, { 'content-type': 'application/json'});
+        res.writeHead(201, { "content-type": "application/json" });
         res.end(JSON.stringify(newNote));
       } catch (err) {
-        res.writeHead(400, {'content-type': 'application/json'});
-        res.end(JSON.stringify({ error: err.message }))
-      }
-    })
-  }
-
-  else if(method === 'DELETE' && path.startsWith('/notes/')){
-    // localhost:3000/notes/43
-    const id = parseInt(path.split('/')[2], 10);
-
-    if(isNaN(id)){
-      res.writeHead(400, {'content-type': 'application/json'});
-      res.end(JSON.stringify({error: 'Invalid ID'}));
-    }
-    const success = deleteNode(id);
-    
-    if (success) {
-      res.writeHead(200, {'content-type': 'application/json'});
-      res.end(JSON.stringify({message: `note with id ${id} - deleted`}));
-    } else {
-      res.writeHead(404, {'content-type': 'application/json'});
-      res.end(JSON.stringify({error: 'Note Not Found'}));
-    }
-  }
-
-  else if(method === 'PUT' && path.startsWith('/notes/')) {
-    const id = parseInt(path.split('/')[2], 10);
-    if(isNaN(id)){
-      res.writeHead(400, {'content-type': 'application/json'});
-      res.end(JSON.stringify({error: 'Invalid ID'}));
-    }
-
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk
-    });
-
-    req.on('end', () => {
-      try {
-        const { title } = JSON.parse(body);
-        if(!title) {
-          throw new Error('Title is required');
-        }
-        const updatedNote = updateNote(id, title);
-
-        if(updatedNote) {
-          res.writeHead(200, {'content-type': 'application/json'});
-          res.end(JSON.stringify(updatedNote));
-        } else {
-          res.writeHead(404, { 'content-type': 'application/json'});
-          res.end(JSON.stringify({ error: 'Note Not Found'}));
-        }
-      } catch (err) {
-        res.writeHead(400, {'content-type': 'application/json'});
+        res.writeHead(400, { "content-type": "application/json" });
         res.end(JSON.stringify({ error: err.message }));
       }
     });
-  } 
-  else {
-    res.writeHead(404, {'content-type': 'application/json'});
-    res.end(JSON.stringify({error: 'Method Not Found'}));  
+  } else if (method === "DELETE" && path.startsWith("/notes/")) {
+    // localhost:3000/notes/43
+    const id = parseInt(path.split("/")[2], 10);
+
+    if (isNaN(id)) {
+      res.writeHead(400, { "content-type": "application/json" });
+      res.end(JSON.stringify({ error: "Invalid ID" }));
+    }
+    const success = deleteNode(id);
+
+    if (success) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: `note with id ${id} - deleted` }));
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ error: "Note Not Found" }));
+    }
+  } else if (method === "PUT" && path.startsWith("/notes/")) {
+    const id = parseInt(path.split("/")[2], 10);
+    if (isNaN(id)) {
+      res.writeHead(400, { "content-type": "application/json" });
+      res.end(JSON.stringify({ error: "Invalid ID" }));
+    }
+
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      try {
+        const { title } = JSON.parse(body);
+        if (!title) {
+          throw new Error("Title is required");
+        }
+        const updatedNote = updateNote(id, title);
+
+        if (updatedNote) {
+          res.writeHead(200, { "content-type": "application/json" });
+          res.end(JSON.stringify(updatedNote));
+        } else {
+          res.writeHead(404, { "content-type": "application/json" });
+          res.end(JSON.stringify({ error: "Note Not Found" }));
+        }
+      } catch (err) {
+        res.writeHead(400, { "content-type": "application/json" });
+        res.end(JSON.stringify({ error: err.message }));
+      }
+    });
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.end(JSON.stringify({ error: "Method Not Found" }));
   }
 });
 
